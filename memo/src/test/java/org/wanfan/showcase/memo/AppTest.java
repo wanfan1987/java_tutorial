b4183d2a04a9d5a555f5ecf457c6b15aa4aebed6
wanfan <hmrseeyou@gmail.com>
2015-06-14 10:25:10 +0800

    test with spring profile, AppTest.java

--- memo/src/test/java/org/wanfan/showcase/memo/AppTest.java
+++ memo/src/test/java/org/wanfan/showcase/memo/AppTest.java
@@ -2,10 +2,12 @@ package org.wanfan.showcase.memo;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
-import org.wanfan.showcase.memo.dao.MemoDao;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.wanfan.showcase.memo.domain.Memo;
 
 /**
@@ -13,17 +15,15 @@ import org.wanfan.showcase.memo.domain.Memo;
  * @author andy
  *
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:spring/application.xml")
+@ActiveProfiles("dev")
 public class AppTest {
 
+	@Autowired
 	private CrudRepository<Memo, String> repository;
 	@Test
-	public void test() {
-		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring/application.xml");
-		try {
-			repository = context.getBean(MemoDao.class);
+	public void testSpringDataJpaConfig() {
 			Assert.assertEquals(0, repository.count());
-		} finally {
-			context.close();
-		}
 	}
 }
